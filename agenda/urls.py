338a34739b3af2889b16a6ac71ff64688ce67495
LuizFPlatini <luizfplatini@gmail.com>
2025-09-06 15:25:00 -0300

    Inclusão da tela de login com autenticação e logout

--- agenda/urls.py
+++ agenda/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from idlelib.pathbrowser import PathBrowser
+
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
@@ -23,5 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('evento/<titulo_evento>', views.evento),
     path('agenda/', views.lista_eventos),
-    path ('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, HttpResponse, redirect
-
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
+@login_required(login_url='/login/')
 def evento (request, titulo_evento):
     try:
         eve = Evento.objects.get(titulo=titulo_evento)
@@ -11,11 +14,32 @@ def evento (request, titulo_evento):
     except Evento.DoesNotExist:
         return HttpResponse('Não existe')
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+
+        return redirect('/')
 
 #    def index(request):
 #    return redirect('/agenda/')
